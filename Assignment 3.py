2afe8eaa3cf6b57d07742c35f2903db413dd1963
VIN4790 <134125166+VIN4790@users.noreply.github.com>
2023-06-03 10:54:28 +0530

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -3,12 +3,28 @@
 
 # In[ ]:
 
-
+Qustion 1
 
 
 
 # In[ ]:
-
+import sys
+def tsum_closet(nums,x,n):
+    closetsum=sys.maxsize
+    nums.sort()
+    for i in range(n-2):
+        pt1=i+1
+        pt2=n-1
+        while(pt1<pt2):
+                sum=nums[i]+nums[pt1]+nums[pt2]
+                if (abs(x-sum)<abs(x-closetsum)):
+                    closetsum=sum
+                if sum>x:
+                    pt2-=1
+                else:
+                    pt1+=1
+                print(closetsum)    
+    return(closetsum)
 
 
 

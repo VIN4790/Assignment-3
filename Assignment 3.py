26695822c3e43e001fe9b4cebd8b1550372f9099
VIN4790 <134125166+VIN4790@users.noreply.github.com>
2023-06-03 14:49:15 +0530

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -56,12 +56,22 @@ def bisearch(nums,target):
 
 # In[ ]:
 
-
+Question 5
 
 
 
 # In[ ]:
-
+def plusone(digits):
+  index=len(digits)-1
+  while index>=0 and digits[index]==9:
+    digits[index]= 0
+    index-=1
+  
+  if index<0:
+    digits.insert(0,1)
+  else:
+    digits[index]+=1
+  return(digits)
 
 
 
